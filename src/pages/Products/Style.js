51d006f1e25e097e47d873066b9b7e7e0c445f1f
dpung1 <pung_12@naver.com>
2023-11-07 14:24:45 +0900

    상품 디테일 페이지 구현 #5

---
+++ src/pages/Products/Style.js
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const SLayout = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 960px;
+`
+export const SContainer = css`
+    width: 700px;
+    height: 850px;
+    display: flex;
+    justify-content: center;
+`
