99bc5e2ce00ec14e60313c359f51e3a6200971c5
hojin <121537196+nugoori@users.noreply.github.com>
2023-11-28 21:08:45 +0900

    hotfix: PageNation.jsx

--- src/utils/PageNation/PageNation.jsx
+++ src/utils/PageNation/PageNation.jsx
@@ -2,6 +2,29 @@ import { useEffect, useState } from "react";
 /** @jsxImportSource @emotion/react */
 import * as S from './Style';
 
+function getTotalPageIndex(startIndex, endIndex) {
+    console.log(startIndex, endIndex)
+    const totalPageIndex = []
+    for(let i = startIndex; i <= endIndex; i++) {
+        totalPageIndex.push(i)
+    }
+    return totalPageIndex;
+}
+
+function getLastPage(showCount, totalCount) {
+    const lastPage = totalCount % showCount === 0 ? totalCount / showCount : Math.floor(totalCount / showCount) + 1;
+    return lastPage;
+}
+
+function getStartIndex(currentPage) {
+    const startIndex = parseInt(currentPage) % 5 === 0 ? parseInt(currentPage) - 4 : parseInt(currentPage) - (parseInt(currentPage) % 5) + 1;
+    return startIndex;
+}
+
+function getEndIndex(startIndex, lastPage) {
+    const endIndex = startIndex + 4 <= lastPage ? startIndex + 4 : lastPage;
+    return endIndex;
+}
 
 const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) => {
 
@@ -10,75 +33,49 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
     const [ selectedPage, setSelectedPage ] = useState(currentPage);
     const [ totalPages, setTotalPages ] = useState([]);
 
-    const lastPage = totalCount % showCount === 0 ? totalCount / showCount : Math.floor(totalCount / showCount) + 1;
+    const lastPage = getLastPage(showCount, totalItemCount);
         
-    const startIndex = parseInt(currentPage) % 5 === 0 ? parseInt(currentPage) - 4 : parseInt(currentPage) - (parseInt(currentPage) % 5) + 1;
-    const endIndex = startIndex + 4 <= lastPage ? startIndex + 4 : lastPage;
+    const startIndex = getStartIndex(currentPage);
 
-
-function getLastPage(totalCount, showCount) {
-    const lastPage = totalCount % showCount === 0 ? totalCount / showCount : Math.floor(totalCount / showCount) + 1;
-    return lastPage;
-}
-
-function getTotalPageIndex(startIndex, endIndex) {
-    const totalPageIndex = []
-    for(let i = startIndex; i <= endIndex; i++) {
-        totalPageIndex.push(i)
-    }
-    return totalPageIndex;
-}
-
-
-const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) => {
-
-    const [ totalCount, setTotalCount ] = useState(0);
-    const [ currentPage, setCurrentPage ] = useState(searchData?.pageIndex);
-    const [ totalPages, setTotalPages ] = useState([]);
-
-    const lastPage = totalCount % showCount === 0 ? totalCount / showCount : Math.floor(totalCount / showCount) + 1;
-        
-    const startIndex = parseInt(currentPage) % 5 === 0 ? parseInt(currentPage) - 4 : parseInt(currentPage) - (parseInt(currentPage) % 5) + 1;
-    const endIndex = startIndex + 4 <= lastPage ? startIndex + 4 : lastPage;
+    const endIndex = getEndIndex(startIndex, lastPage);
 
     const totalPageIndex = getTotalPageIndex(startIndex, endIndex);
-    // 전체 페이지 갯수 => 문제 없음
+
     useEffect(() => {
-        setTotalCount(totalItemCount)
+        setTotalCount(totalItemCount)   
     }, [totalItemCount])
-
-
-    // 카테고리 변경 시 페이지 1페이지로, 전체 페이지 갯수 초기화
-
-    useEffect(() => {        
-        // 1페이지로
-        setCurrentPage(1)
-
-        // 전체 페이지 갯수 초기화
-        setTotalCount(totalItemCount)
-
-        setSearchData({...searchData, pageIndex: 1});
-    }, [searchData.petTypeName]);
-
     
-
     useEffect(() => {
         setTotalPages(totalPageIndex);
+    }, [searchData.productCategoryName]);
+    
+    useEffect(() => {
+        setSelectedPage(currentPage);
+    }, [currentPage]);
+    
+    useEffect(() => {
         if(searchData.searchValue !== "") {
             setCurrentPage(totalPages[0]);
         } else {
             setCurrentPage(currentPage)
         }
-    }, [])
+    }, [searchData.searchValue])
     
-
     const handlePageClick = (page) => {
         setCurrentPage(page);
     }
-
-
-
-
+    
+    useEffect(() => {
+        const newSearchData = {...searchData, pageIndex: currentPage};
+        setSearchData(newSearchData);
+    }, [currentPage]);
+    
+    useEffect(() => {
+        const newSearchData = {...searchData, pageIndex: 1};
+        setSearchData(newSearchData);
+        setCurrentPage(1)
+    }, [searchData.productCategoryName])
+    
     return (
         <div>
             <button
@@ -90,7 +87,7 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
             
             {totalPageIndex.map((page, index) => (
                 <button 
-                    css={currentPage === page ? S.selectedPageButton : S.PageButton}
+                    css={selectedPage === page ? S.selectedPageButton : S.PageButton}
                     name="totalPageIndex"
                     onClick={() => handlePageClick(page)}
                     key={index}>
@@ -107,5 +104,5 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
         </div>
     )
 }
-}
+
 export default PageNation;
