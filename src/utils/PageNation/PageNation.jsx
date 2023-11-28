697a5cd1aef958261563a4be3319853586a020bf
moveold0611 <137735075+moveold0611@users.noreply.github.com>
2023-11-28 16:45:06 +0900

    hotfix PageNation.jsx

--- src/utils/PageNation/PageNation.jsx
+++ src/utils/PageNation/PageNation.jsx
@@ -36,15 +36,12 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
     const [ currentPage, setCurrentPage ] = useState(searchData?.pageIndex);
     const [ totalPages, setTotalPages ] = useState([]);
 
-    const lastPage = getLastPage(totalCount, showCount);
+    const lastPage = totalCount % showCount === 0 ? totalCount / showCount : Math.floor(totalCount / showCount) + 1;
+        
+    const startIndex = parseInt(currentPage) % 5 === 0 ? parseInt(currentPage) - 4 : parseInt(currentPage) - (parseInt(currentPage) % 5) + 1;
+    const endIndex = startIndex + 4 <= lastPage ? startIndex + 4 : lastPage;
 
-    const startIndex = getStartIndex(currentPage)
-
-    const endIndex = getEndIndex(startIndex, lastPage);
-
-    const totalPageIndex = getTotalPageIndex(startIndex, endIndex)
-
-
+    const totalPageIndex = getTotalPageIndex(startIndex, endIndex);
     // 전체 페이지 갯수 => 문제 없음
     useEffect(() => {
         setTotalCount(totalItemCount)
@@ -62,9 +59,7 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
 
         setSearchData({...searchData, pageIndex: 1});
     }, [searchData.petTypeName]);
-  
-        setSelectedPage(currentPage);
-    }, [currentPage]);
+
     
 
     useEffect(() => {
@@ -112,5 +107,5 @@ const PageNation = ({ showCount, totalItemCount, searchData, setSearchData }) =>
         </div>
     )
 }
-
+}
 export default PageNation;
